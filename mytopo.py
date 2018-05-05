142bea9e4739dda4d4e36c8350e91b8124fbab31
root <lee078bg@yahoo.com>
2018-05-04 18:21:43 -0700

    Added custom topology for the QoS.  Project is mostly complete though we still need the presentation

--- mytopo.py
+++ mytopo.py
@@ -1,22 +1,17 @@
 from mininet.topo import Topo
 
 class MyTopo(Topo):
+    def __init__(self):
+        Topo.__init__(self)
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        s1 = self.addSwitch('s1')
 
-        def __init__(self):
-                Topo.__init__(self)
-                host1 = self.addHost('h1',ip="172.16.20.10",defaultRoute = "via 172.16.20.1")
-                host2 = self.addHost('h2',ip="172.16.10.10",defaultRoute = "via 172.16.20.1")
-                host3 = self.addHost('h3',ip="172.16.30.10",defaultRoute = "via 172.168.20.1")
-                host4 = self.addHost('h4',ip="172.16.40.10",defaultRoute = "via 172.168.20.1")
-                switch1 = self.addSwitch('s1')
-                switch2 = self.addSwitch('s2')
-
-                self.addLink(switch1, host1)
-                self.addLink(switch2, host3)
-                self.addLink(switch2, host4)
-                self.addLink(switch2, host2)
-                self.addLink(switch1, switch2)
-
+        self.addLink(h1, s1)
+        self.addLink(s1, h2)
+        self.addLink(s1, h3)
+        self.addLink(s1, h4)
+        
 topos = {'mytopo':(lambda:MyTopo())}
-
-
